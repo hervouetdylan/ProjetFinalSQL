76e0fef27811bcbda141ce26d4e1263a909a0de5
AROLLAND1 <alexandre.rolland85@ynov.com>
2023-01-17 16:13:33 +0100

    init on express

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    }
-);
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname + "/views"));
+
+
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-    }
-);
+    console.log("Serveur démarré (http://localhost:3000/) !");
+});
+
+// GET /
+app.get("/", (req, res) => {
+    // res.send("Bonjour le monde...");
+    res.render("index");
+  });
+  
+  // GET /about
+  app.get("/about", (req, res) => {
+    res.render("about");
+  });
+  
+  // GET /data
+  app.get("/data", (req, res) => {
+    const test = {
+      titre: "Test",
+      items: ["un", "deux", "trois"]
+    };
+    res.render("data", { model: test });
+  });
