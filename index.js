410ec8c8d959406a50d245754b26dd73939c96e5
AROLLAND1 <alexandre.rolland85@ynov.com>
2023-01-24 13:58:12 +0100

    add css path

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ app.set("view engine", "ejs");
 
 app.set("views", path.join(__dirname + "/views"));
 
+app.use(express.static(path.join(__dirname, '/public')))
 
 
-app.listen(3000, () => {
-    console.log("Serveur démarré (http://localhost:3000/) !");
+
+app.listen(8080, () => {
+    console.log("Serveur démarré (http://localhost:8080/) !");
 });
 
 // GET /
