8ea464a3494e3070f523007cdae8f76daf05d1c7
AROLLAND1 <alexandre.rolland85@ynov.com>
2023-01-31 03:27:38 +0100

    add filter

--- public/js/employes.js
+++ public/js/employes.js
@@ -1,8 +1,30 @@
 const selectSorter = document.getElementById('sort');
+const searchBar = document.getElementById('searchBar');
 
-selectSorter.addEventListener('click', function() {
+searchBar.addEventListener('keyup', () => {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    filter = searchBar.value.toUpperCase();
+    table = document.getElementById("employesTable");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+});
+
+selectSorter.addEventListener('click', function () {
     sortTable(selectSorter.value);
-}); 
+});
 
 function sortTable(n) {
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
